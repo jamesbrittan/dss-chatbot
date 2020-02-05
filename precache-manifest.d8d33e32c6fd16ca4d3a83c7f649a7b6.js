2e088267f81d10cb4a98bf79ae8829c47a76b786
James Brittan <jamesbrittan@gmail.com>
2020-02-05 13:59:45 +0000

    Updates

---
+++ precache-manifest.d8d33e32c6fd16ca4d3a83c7f649a7b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ad90979b7644c4b89752977f911654",
+    "url": "/dss-chatbot/index.html"
+  },
+  {
+    "revision": "fcdc3a1e120a7f79b7c4",
+    "url": "/dss-chatbot/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6cf54a09856bd864d44e",
+    "url": "/dss-chatbot/static/js/2.9c35800b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/dss-chatbot/static/js/2.9c35800b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fcdc3a1e120a7f79b7c4",
+    "url": "/dss-chatbot/static/js/main.b8f584e4.chunk.js"
+  },
+  {
+    "revision": "71eaf1d24d07eb07f7cc",
+    "url": "/dss-chatbot/static/js/runtime-main.9c75a18c.js"
+  }
+]);
