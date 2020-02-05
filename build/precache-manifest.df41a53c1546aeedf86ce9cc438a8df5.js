952e76bf005184fcce9430da00e49882c1ea6d59
James Brittan <jamesbrittan@gmail.com>
2020-02-05 15:34:35 +0000

    adding build folder

---
+++ build/precache-manifest.df41a53c1546aeedf86ce9cc438a8df5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3c2e9ab974e428501f37e9a8424b47",
+    "url": "/dss-chatbot/index.html"
+  },
+  {
+    "revision": "b087755f023aae8a6a3a",
+    "url": "/dss-chatbot/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3fe4f86eae8091054bca",
+    "url": "/dss-chatbot/static/js/2.1fcbe31a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/dss-chatbot/static/js/2.1fcbe31a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b087755f023aae8a6a3a",
+    "url": "/dss-chatbot/static/js/main.f2787356.chunk.js"
+  },
+  {
+    "revision": "6c016bb9887d4fcc04eb",
+    "url": "/dss-chatbot/static/js/runtime-main.757cf84a.js"
+  }
+]);
